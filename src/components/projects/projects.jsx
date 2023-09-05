264310a79f2a86a3ba7b7de54f7126b8e4e95ed3
erikjearl <erikjearl@gmail.com>
2023-09-05 15:49:53 -0400

    projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -8,16 +8,16 @@ function Projects() {
 
   const projectData = [
     {
-      name:'project 1',
-      icon:'./images/spartanShare.png',
+      name:'File Sharing Website - Team Lead',
+      icon:spartanShare,
       bullets:[
-        'bullet 1',
-        'b2',
-        "3"
+        'Developed a full-stack web application for Case Western students to upload and share class notes and files using React, Node JS, Express JS, MongoDB, Mongoose, Redux and Material UI',
+        'Implemented CRUD functionality using RESTful API design patterns and handled site URL routing',
+        "Lead team of 4: organized meetings, delegated tasks and maintained version control through Git"
       ],
       skills:[
-        'skill1',
-        's2',
+        'Front End Development - React JS, Redux and Material UI',
+        'Back End Development - Node JS, Express JS, Mongo DB, Mongoose',
       ]
       
     },
@@ -29,7 +29,7 @@ function Projects() {
         '2b2',
       ],
       skills:[
-        'skill1 aaa',
+        '',
         's2 aaaa',
       ]
     },
@@ -50,28 +50,43 @@ function Projects() {
         </div>
 
         {projectData.map((project, index) => (
-          <Card key={index} style={{ backgroundColor: '#eee', color:'black', margin: '5px' }}>
-              <CardHeader title={project.name} />
-              <CardContent>
-                <span>Description:</span>
-                <ul style={{ margin: 0, paddingBottom:5}}>
-                  {project.bullets.map((bullet, index) => (
-                    <li key={index}>{bullet}</li>
-                  ))}
-                </ul>
-                <span>Skills:</span>
-                <ul style={{ margin: 0, paddingBottom:5}}>
-                  {project.skills.map((bullet, index) => (
-                    <li key={index}>{bullet}</li>
-                  ))}
-                </ul>
-              </CardContent>
-              <CardMedia
-                style={{ width: 100, height: 100, margin: '10px'}}
-                image={project.icon}
-                title={project.name}
-              />
-          </Card>
+          <Card key={index} style={{ display: 'flex', flexDirection: 'column', backgroundColor: '#eee', color: 'black', margin: '5px' }}>
+  {/* CardHeader */}
+  <CardHeader title={<b>{project.name}</b>} style={{ backgroundColor: '#eee', color: 'black', paddingBottom:'0px', marginBottom:'0px'}} />
+  
+  {/* Content and Media Container */}
+  <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+    {/* CardContent */}
+    <div style={{ flex: 1, padding: '0px 10px 10px 10px', marginTop:'0px'}}>
+      <CardContent style={{ paddingBottom: 20 }}>
+        <ul style={{ margin: 0, paddingBottom: 5 }}>
+          {project.bullets.map((bullet, index) => (
+            <li key={index}>{bullet}</li>
+          ))}
+        </ul>
+        <span style={{ fontWeight: '500', fontSize: '18px', fontStyle: 'italic' }}>Skills Acquired</span>
+        <ul style={{ margin: 0, paddingBottom: 5 }}>
+          {project.skills.map((bullet, index) => (
+            <li key={index}>{bullet}</li>
+          ))}
+        </ul>
+      </CardContent>
+    </div>
+    
+    {/* CardMedia */}
+    <div style={{ padding: '10px' }}>
+      <CardMedia
+        style={{ width: 300, height: 300 }}
+        image={project.icon}
+        title={project.name}
+        alt={project.name}
+      />
+    </div>
+  </div>
+</Card>
+
+
+
         ))}
 
         </Paper>
